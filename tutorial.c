1234f7ed4fe49bc3d9cb359883e273a2feeb31ce
wayared <jarecast@espol.edu.ec>
2023-04-11 23:37:05 -0700

    How to use GDB

--- tutorial.c
+++ tutorial.c
@@ -6,4 +6,5 @@
 // 4. break 13(numero de linea) | sirve para detener nuestro programa en la linea que queramos
 // 5. next | sirve para continuar nuestro prog linea x linea , para no colocar break 14 despues de un break 13
 // 6. refresh | sirve para volver al punto anterior
-// 7. display <variable> | en toda linea se va apoder visualizar el valor de la variable 
+// 7. display <variable> | en toda linea se va apoder visualizar el valor de la variable
+//  
